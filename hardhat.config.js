b9901b64666ad933aa2b6d66ac5f9c1aa830bc9a
KvPradeepthi <153480628+KvPradeepthi@users.noreply.github.com>
2025-12-12 06:00:33 +0530

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-gas-reporter");
+
+module.exports = {
+  solidity: {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS === "true",
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    artifacts: "./artifacts",
+  },
+};
